381c6ed4a9116f3899e6f8637a3c1cc2262c66eb
Alfian57 <alfiangadingsaputra@gmail.com>
2025-10-19 14:47:09 +0700

    chore: add and setup riverpod

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:insightmind/features/insightmind/presentation/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'InsightMind',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo, useMaterial3: true),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/features/insightmind/presentation/pages/screening_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScreeningPage extends StatelessWidget {
+  const ScreeningPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Buat Screening Page Disini')));
+  }
+}
